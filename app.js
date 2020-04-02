09a57ebaa30a4fbff34e5565cc7645b25383ff60
Arimit Bhattacharya <arimitb@gmail.com>
2020-04-02 12:34:01 +0200

    Added comments.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3001;
 
 app.use(express.static('public'))
 
+// Routing
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/whip.html')
 })
@@ -14,12 +16,18 @@ app.get('/controller', (req, res) => {
 	res.sendFile(__dirname + '/views/whip-controller.html')
 })
 
+// Listens for any new connections
+
 io.on('connection', socket => {
 	console.log('New connection.')
+
+	// Re-emits gyroscope data to controller with UID
 
 	socket.on('whipControllerOutput', data => {
 		io.emit(`whipControllerInput-${data.id}`, data.reading)
 	})
+
+	// Re-emits parameter changes to controller with UID
 
 	socket.on('whipControllerParameters', data => {
 		io.emit(`whipControllerParameters-${data.id}`, data.params)
