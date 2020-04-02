ccf43971c8f2dd2fcb597378156d800203e715c6
Arimit Bhattacharya <arimitb@gmail.com>
2020-04-02 12:31:43 +0200

    Added comments.

--- public/js/whip-controller.js
+++ public/js/whip-controller.js
@@ -4,6 +4,8 @@ $(() => {
 	let acceleration = 0
 	const readingEl = $('.reading')
 
+	// Get UID from URL
+
 	const getQueryParams = (params, url) => {
 		let href = url
 		let reg = new RegExp( '[?&]' + params + '=([^&#]*)', 'i' )
@@ -13,10 +15,15 @@ $(() => {
 
 	id = getQueryParams('id', window.location.href)
 
+	// Listen for change in device orientation
+
 	window.addEventListener('deviceorientation', e => {
 		if (e.beta !== null && e.beta !== undefined) {
+			// Implements correction since beta may jump from -180 to 180 and vice-versa
 			acceleration = e.beta + (e.beta > 90 ? -180 : (e.beta < -90 ? 180 : 0))
 			
+			// Emits the current reading
+
 			socket.emit('whipControllerOutput', {
 				id: id,
 				reading: acceleration
@@ -25,10 +32,14 @@ $(() => {
 		}
 	}, true)
 
+	// Updates the reading on the mobile DOM
+
 	function updateReading() {
 		$(readingEl).html(parseInt(acceleration) + '°')
 	}
 
+	// Communicates any changes in parameters to the desktop client
+	
 	function updateParameters() {
 		let params = {}
 		let settings = $('.settings-item')
